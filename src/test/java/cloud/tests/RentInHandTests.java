9be99bafa12c91d25588e056472f947c10e333c3
mazafakinshit <mazafakinshit@yandex.ru>
2021-02-06 18:11:01 +0300

    added some tests

--- src/test/java/cloud/tests/RentInHandTests.java
+++ src/test/java/cloud/tests/RentInHandTests.java
@@ -16,8 +16,7 @@ import cloud.autotests.utils.FileUtils;
 
 import java.io.File;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
@@ -44,7 +43,7 @@ public class RentInHandTests extends TestBase {
     void checkSiteCanBeFound(){
 
         step("Заходим на сайт", () -> {
-        open("https://rentinhand.ru/");
+        open("https://staging.rentinhand.ru/");
         });
         step("Проверяем наличие текста Rent in hand", () -> {
         $("html").shouldHave(text("Rent in Hand"));
@@ -56,7 +55,7 @@ public class RentInHandTests extends TestBase {
     @Description("Проверяем работоспособность кнопок навигационной панели")
     void checkButtons() {
         step("Заходим на сайт", () -> {
-            open("https://rentinhand.ru/");
+            open("https://staging.rentinhand.ru/");
         });
         step("Проверяем наличие текста Rent in hand", () -> {
             $("html").shouldHave(text("Rent in Hand"));
@@ -65,79 +64,105 @@ public class RentInHandTests extends TestBase {
             $(byText("Возможности")).click();
         });
         step("Проверяем скролл до раздела Возможности", () -> {
-            $(".col-lg-12").shouldHave(text(" это единая система управления вашим прокатным бизнесом"));
+            $(".col-lg-12").shouldHave(text("это единая система управления вашим прокатным бизнесом"));
         });
         step("Нажимаем на кнопку: Тарифы", () -> {
             $(byText("Тарифы")).click();
         });
         step("Проверяем наличие раздела Тарифы", () -> {
-            $(".f-18").shouldHave(text("Мы предлагем минимальные цены для наших пользовтаелей"));
+            $("html").shouldHave(text("Мы предлагаем минимальные цены для наших пользователей"));
         });
         step("Нажимаем на кнопку: Расширения", () -> {
             $(byText("Расширения")).click();
         });
         step("Проверяем наличие раздела Расширения", () -> {
-            $("html").shouldHave(text("Мы предлагем минимальные цены для наших пользовтаелей"));
+            $("#modules").shouldHave(text("мы разработали несколько необходимых функциональных модулей"));
         });
         step("Нажимаем на кнопку: Контакты", () -> {
             $(byText("Контакты")).click();
         });
         step("Проверяем наличие раздела Контакты", () -> {
-            $("html").shouldHave(text("Контактная информация"));
+            $("#contacts").shouldHave(text("Контакты"));
         });
 
         step("Проверяем работоспособность кнопок Подробнее в разделе Расширения", () -> {
             $(byText("Расширения")).click();
             $$(byText("Подробнее")).get(0).click();
-            $("html").shouldHave(text("Магазин"));
             sleep(2000);
+            $("html").shouldHave(text("Магазин"));
             $(byText("Расширения")).click();
             $$(byText("Подробнее")).get(1).click();
-            $("html").shouldHave(text("Мастерская"));
             sleep(2000);
+            $("html").shouldHave(text("Мастерская"));
+
         });
 
         step("Проверяем работоспособность кнопок выбора срока использования продукта в разделе Тарифы", () -> {
             $(byText("Тарифы")).click();
-            $(byText("1 месяц")).click();
-            $("html").shouldHave(text("Старт"),text("1000"));
-            $(byText("6 месяцев")).click();
-            $("html").shouldHave(text("Старт"),text("750"));
-            $(byText("12 месяцев")).click();
-            $("html").shouldHave(text("Старт"),text("500"));
+            $$(".nav-link").findBy(text("1 месяц")).click();
+            $$(".col-lg-4").get(0).shouldHave(text("1000"));
+            $$(".nav-link").findBy(text("6 месяцев")).click();
+            $$(".col-lg-4").get(1).shouldHave(text("750"));
+            $$(".nav-link").findBy(text("12 месяцев")).click();
+            $$(".col-lg-4").get(2).shouldHave(text("500"));
         });
 
         step("Проверяем работоспособность кнопок Попробовать бесплатно 14 дней", () -> {
             $(".navbar-brand").click();
             $$(byText("Попробовать бесплатно 14 дней")).get(1).click();
             sleep(1000);
-            $("#form-registration").shouldHave(text("Персональные данные"));
+            $("#page-login").shouldHave(text("Регистрация"));
             $(".navbar-brand").click();
             $$(byText("Попробовать бесплатно 14 дней")).get(2).click();
             sleep(1000);
-            $("#form-registration").shouldHave(text("Персональные данные"));
+            $("#page-login").shouldHave(text("Регистрация"));
             $(".navbar-brand").click();
             $$(byText("Попробовать бесплатно 14 дней")).get(3).click();
             sleep(1000);
-            $("#form-registration").shouldHave(text("Персональные данные"));
-        });
-
-        step("Проверка работоспособности ссылок Подробнее", () -> {
+            $("#page-login").shouldHave(text("Регистрация"));
             $(".navbar-brand").click();
-            $(byText("Административная веб-панель")).scrollIntoView(true);
-            $(byPartialLinkText("Подробнее")).click();
-            $("#page-top").shouldHave(text("Административная панель включает в себя следующие разделы"));
-            $(".navbar-brand").click();
-            $(byText("Мобильное приложение")).scrollIntoView(true);
-            $(byPartialLinkText("Подробнее")).click();
-            $("#page-top").shouldHave(text("один из способов облегчить работу на точках проката и продаж"));
         });
 
     }
 
     @Test
+    @Description("Проверка ссылок на сайте проекта")
+    void checkLinks(){
+
+        step("Заходим на сайт", () -> {
+            open("https://staging.rentinhand.ru/");
+        });
+        step("Проверяем работоспособность ссылок Подробнее", () -> {
+            $(byText("Административная веб-панель")).scrollIntoView(true);
+            $$(byPartialLinkText("Подробнее")).get(0).click();
+            $("#page-top").shouldHave(text("веб-панель которая открывается из любого браузера"));
+            $(".navbar-brand").click();
+            $(byText("Мобильное приложение")).scrollIntoView(true);
+            $$(byPartialLinkText("Подробнее")).get(1).click();
+            $("#page-top").shouldHave(text("один из способов облегчить работу на точках проката и продаж"));
+        });
+
+        step("Проверяем работоспособность ссылок в разделе Информация", () -> {
+            $(".navbar-brand").click();
+            $(byText("Справочное руководство")).click();
+            sleep(3000);
+            $("#features").shouldHave(text("Справочное руководство"));
+            $(".navbar-brand").click();
+            $(byText("Помощь")).click();
+            $("#features").shouldHave(text("Лицензионный договор-офферта"));
+            $(".navbar-brand").click();
+            $(byText("Пользовательское соглашение")).click();
+            $("h3").shouldHave(text("Пользовательское соглашение"));
+            $(".navbar-brand").click();
+            $(byText("Политика конфиденциальности")).click();
+            $("h3").shouldHave(text("Политика конфиденциальности"));
+            $(".navbar-brand").click();
+        });
+    }
+
+    @Test
     @Description("Проверка авторизации на сайте проекта")
-    void checkAuthorizationForm(){
+    void checkAuthorizationForm() {
 
         step("Заходим в раздел авторизации", () -> {
             open("https://rentinhand.ru/");
